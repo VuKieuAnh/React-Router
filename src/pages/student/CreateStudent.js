64b87c65c41b5ed9ce28e07a2b585ab6012cb939
VuKieuAnh <vukieuanh.hnue@gmail.com>
2023-02-15 11:15:49 +0700

    add, list user

--- src/pages/student/CreateStudent.js
+++ src/pages/student/CreateStudent.js
@@ -1,14 +1,45 @@
 import {useNavigate} from "react-router-dom";
+import {Field, Form, Formik} from "formik";
+import axios from "axios";
 
 export default function CreateStudent(){
     const navigate = useNavigate()
     return(
         <>
             <h1>Tao moi HV</h1>
-            <button onClick={()=>{navigate(
-                '/list-student',
-                {state:{id: 'ID01',name: 'KA' }}
-            )}}>Create</button>
+            <Formik initialValues={{
+                id:'',
+                name: 'KAX',
+                username: '',
+                email: ''
+
+            }} onSubmit={(values)=>{
+                axios.post('https://jsonplaceholder.typicode.com/users', values)
+                    .then(
+                        axios.get('https://jsonplaceholder.typicode.com/users').then(
+                            res =>{
+                                console.log(res.data)
+                            }
+                        ),
+                        alert("thanh cong")
+                    )
+            }
+
+            }>
+                <Form>
+                    <Field name={'id'} placeholder={'id'}></Field>
+                    <Field name={'name'} placeholder={'name'}></Field>
+                    <Field name={'username'} placeholder={'username'}></Field>
+                    <Field name={'email'} placeholder={'email'}></Field>
+                    <button >Save</button>
+                </Form>
+            </Formik>
+            
+            
+            {/*<button onClick={()=>{navigate(*/}
+            {/*    '/list-student',*/}
+            {/*    {state:{id: 'ID01',name: 'KA' }}*/}
+            {/*)}}>Create</button>*/}
         </>
     )
 }
