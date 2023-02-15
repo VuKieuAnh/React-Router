03f6a2b42b8f4272de646f014320466e5844998e
VuKieuAnh <vukieuanh.hnue@gmail.com>
2023-02-15 10:41:05 +0700

    add list

--- src/pages/student/ListStudent.js
+++ src/pages/student/ListStudent.js
@@ -1,11 +1,33 @@
 import {useLocation} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function ListStudent(){
-    const {state} = useLocation();
-    console.log(state);
+    // const {state} = useLocation();
+    // console.log(state);
+    const [list, setList] = useState([]);
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/users').then(
+            res =>{
+                setList(res.data)
+            }
+        )
+    })
     return(
         <>
             <h1>Danh sach HV</h1>
+            <ul>
+                {list.map((item, key)=>{
+                    return (
+                        <li key={key}>
+                            {item.name},
+                            {item.username},
+                            {item.email}
+                        </li>
+                    )
+                })}
+            </ul>
+
         </>
     )
 }
